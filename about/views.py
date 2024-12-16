bbeb63d72fd11af844d7dd006b3d8a0862cbb809
Lorraine Scanlon <lorrainescanlon@gmail.com>
2024-12-16 22:01:59 +0000

    updated hello views.py file

--- about/views.py
+++ about/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def about(request):
+def about_me(request):
     return HttpResponse("This would be the about page")
 
 
